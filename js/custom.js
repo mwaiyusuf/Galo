fb9d56f7dc92d913875f833ba69fd82fbd34eb31
mohammed yusuf <mwaigalo5@gmail.com>
2018-07-02 12:37:11 +0300

    t

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@
     f=prompt ("History grade", 0)
     g=prompt("Spanish grade",0)
     ave=(parseFloat(d)+ parseFloat(e) + parseFloat(f) + parseFloat(g))/4
-//
+
 //    if (ave>=75)
 //{
 //    h="You passed!"
